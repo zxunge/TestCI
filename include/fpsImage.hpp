f7b0ed0b8b78f84d9ce1a1ef63d2b40f9df72b03
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-08-07 09:49:49 +0800

    Update.

--- include/fpsImage.hpp
+++ include/fpsImage.hpp
@@ -1,5 +1,11 @@
 #include <wx/image.h>
 #include <wx/dir.h>
+#include <wx/gdicmn.h>
+#include <vector>
+
+using std::vector;
+
+enum class fpsSplitMode { byCounts, byPixel };
 
 class fpsImage : private wxImage
 {
@@ -7,8 +13,10 @@ public:
     fpsImage();
     fpsImage(wxString path) {m_image.Open(path)}
     ~fpsImage() {m_image.Close()}
-    static 
-    
+    static vector<vector<wxRect>> GetOutputList(int rowsOrHeight, int colsOrWidth, fpsSplitMode splitMode = fpsSplitMode::byCounts);
+    void SetOutputList(vector<vector<wxRect>> list);
+    int Split();
+
 private:
     wxImage m_image;
 }
